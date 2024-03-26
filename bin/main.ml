7fea2d9341373953f0df6353f5837c18a8929e0f
Mericel <mst223@cornell.edu>
2024-03-25 21:42:51 -0400

    Creates basic implementation
    - Separates game into UI (main) and functionality (game)
    - Added formatting

--- bin/main.ml
+++ bin/main.ml
@@ -1,42 +1,40 @@
-let letters = [ 'e'; 'a'; 'r'; 't'; 'h'; 's'; 'o' ]
-let central_letter = 'e'
+(* Initializes game. Also saves important information in tuple [game_st]*)
+let game_st = Cs3110_final_project.Game.initialize ()
 
-let valid_words =
-  [
-    "earth";
-    "heart";
-    "rate";
-    "hate";
-    "heat";
-    "eat";
-    "tea";
-    "sea";
-    "ear";
-    "tear";
-    "shear";
-  ]
+(** returns the first entry of the tuple [st] *)
+let fst st =
+  match st with
+  | x, _ -> x
 
-let is_valid_word word =
-  let has_central_letter = String.contains word central_letter in
-  let uses_valid_letters = String.for_all (fun c -> List.mem c letters) word in
-  let is_known_word = List.mem word valid_words in
-  has_central_letter && uses_valid_letters && is_known_word
+(** returns the second entry of the tuple [st] *)
+let snd st =
+  match st with
+  | _, x -> x
 
+(** [game_loop ()] asks for a guess and checks for validity. It ends the game
+    when the user types "quit" *)
 let rec game_loop () =
   print_endline "Enter a word (or 'quit' to exit): ";
   let user_word = read_line () in
   if user_word = "quit" then print_endline "Goodbye!"
   else (
-    if is_valid_word user_word then print_endline "Valid word!"
+    if
+      Cs3110_final_project.Game.is_valid_word user_word (snd game_st)
+        (fst game_st) Cs3110_final_project.Game.word_list
+    then print_endline "Valid word!"
     else print_endline "Invalid word.";
     game_loop ())
 
+(** Starts the game by printing all necessary information *)
 let () =
-  let letters_string = List.map (String.make 1) letters |> String.concat "" in
+  Random.self_init ();
+  let letters_string =
+    List.map (String.make 1) (fst game_st) |> String.concat ""
+  in
   print_endline
     ("Welcome to the Spelling Bee Game!\nUse these letters to create words: "
    ^ letters_string);
   print_endline
     ("The central letter that must be in every word is: "
-    ^ String.make 1 central_letter);
+    ^ String.make 1 (snd game_st));
   game_loop ()
