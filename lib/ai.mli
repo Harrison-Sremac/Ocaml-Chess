5451782ded3f69536e0dac22265b0bac466b193c
mericeltao <mst223@cornell.edu>
2024-05-15 22:56:40 -0700

    Deleted unneccessary files

--- lib/ai.mli
+++
@@ -1,6 +0,0 @@
-(* Represents the difficulty level or depth of the AI's search *)
-type difficulty = int
-
-(* Evaluates the board and returns the best move for the AI *)
-val best_move :
-  Board.board -> Types.color -> difficulty -> Board.position * Board.position
